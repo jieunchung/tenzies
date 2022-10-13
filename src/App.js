28f57fafac0ee8f37cfb335c071ebc4cda5d2b2e
jieunchung <106100696+jieunchung@users.noreply.github.com>
2022-10-13 09:36:22 +0200

    Generate array of 10 random numbers

--- src/App.js
+++ src/App.js
@@ -2,22 +2,23 @@ import React from "react";
 import Die from "./components/Die";
 
 /**
- * Challenge:
  *
- * - Create a Die component that takes a `value` prop
- * - Render 10 instances of the Die component (manually)
- *      - Provide a number between 1-6 for the value on each
- *        for now
- * - Style the <main> and <Die> components
- *   to look like they do in the slide
- *      - Hints: Create a container to hold the 10 instances
- *        of the Die component, and use CSS Grid to lay them
- *        out evenly in 2 rows of 5 columns
- *      - Use flexbox on main to center the dice container
- *        in the center of the page
+ * Write a function that returns an array
+ * of 10 random numbers between 1-6 inclusive.
+ *
+ * Log the array of numbers to the console for now
  */
 
 const App = () => {
+  const allNewDice = () => {
+    const numberArray = [];
+    for (let i = 0; i < 10; i++) {
+      numberArray.push(Math.floor(Math.random() * 6) + 1);
+    }
+    return numberArray;
+  };
+  console.log(allNewDice());
+
   return (
     <main>
       <div className="game">
